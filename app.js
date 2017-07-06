502f8fd2cea563f0a7f4fc3533779df5471fe757
wickedpaper <dugoma@conciergedevelopment.com>
2017-07-06 00:54:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ var cfenv = require("cfenv");
 var path = require('path');
 var cors = require('cors');
 
+// Setup the required environment variables
+var vcapLocal = null;
+try {
+  vcapLocal = require("./vcap-local.json");
+}
+catch (e) {}
+
+var appEnvOpts = vcapLocal ? {vcap:vcapLocal} : {};
+var appEnv = cfenv.getAppEnv(appEnvOpts);
+
+try {
+	cloudantService = appEnv.services.cloudantNoSQLDB[0];
+}
+catch (e) {
+	console.error("Error looking up service: ", e);
+}
+
 // Setup route handlers
-var policies = require('./routes/policies');
+var desc = require('./routes/desc');
 
 // Setup express middleware.
 var app = express();
@@ -15,17 +32,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'www')));
 
 // REST HTTP Methods
-app.get('/db/:option', policies.dbOptions);
-app.get('/policies', policies.list);
-app.get('/fib', policies.fib);
-app.get('/loadTest', policies.loadTest);
-app.get('/policies/:id', policies.find);
-app.post('/policies', policies.create);
-app.put('/policies/:id', policies.update);
-app.delete('/policies/:id', policies.remove);
+app.get('/db/:option', desc.dbOptions);
+app.get('/policies', desc.list);
+app.get('/fib', desc.fib);
+app.get('/loadTest', desc.loadTest);
+app.get('/policies/:id', desc.find);
+app.post('/policies', desc.create);
+app.put('/policies/:id', desc.update);
+app.delete('/policies/:id', desc.remove);
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, "0.0.0.0", function () {
   // print a message when the server starts listening
-  console.log("catalog server starting on " + appEnv.url);
+  console.log("desc server starting on " + appEnv.url);
 });
